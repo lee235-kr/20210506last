3225ab3d5ee3a3eed605ab0ace14b2c20fe8b5b3
이상오 <@DESKTOP-MFVLMB5>
2021-05-06 15:55:26 +0900

    20210506

---
+++ Smart11/src/package1/Ex011.java
@@ -0,0 +1,21 @@
+package package1;
+
+public class Ex011 {
+
+	int d1;
+	int d2;
+	int g1;
+	int g2;
+	
+	public void gugu(int d1,int d2,int g1,int g2) {
+		for(int i=d1;i<=d2;i++) {
+			for(int j =g1;j<=g2;j++) {
+				System.out.println(i+"*"+j+"="+i*j);
+			}
+			System.out.println();
+		}
+		
+	}
+	
+	
+}
---
+++ Smart11/src/package1/Ex012.java
@@ -0,0 +1,11 @@
+package package1;
+
+public class Ex012 {
+
+	public static void main(String[] args) {
+		Ex011 ex=new Ex011();
+		ex.gugu(1, 4, 1, 9);
+		
+	}
+
+}
